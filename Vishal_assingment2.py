73d544320acf2e5fd7208031ed75747b57d9503c
vishal <vishalgaurav.iitp@gmail.com>
2016-08-26 11:21:03 +0900

    assingment 2

---
+++ Vishal_assingment2.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# # Lab Retreat
+# 
+# ### Vishal Guarav
+
+# In[4]:
+
+#import the necessary modules
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+import pylab
+get_ipython().magic(u'matplotlib notebook')
+
+
+# In[5]:
+
+x = pylab.rand(1000)
+
+
+# In[9]:
+
+plt.hist(x,bins=10)
+plt.savefig('vishal_assingment2')
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
